fae1639f0429edd90c7db4618e145a53d6fa396b
Andres <andrecitoasd@gmail.com>
2019-11-18 16:24:55 +0100

    generico push

--- server/controllers/adminPanel.js
+++ server/controllers/adminPanel.js
@@ -24,7 +24,6 @@ function adminVerification(req, res) {
 function getAllPosts(req, res) {
     var orderBy = req.body.orderBy;
     var orderOption = req.body.orderOption;
-    console.log(orderBy + '  ' + orderOption);
     posts.sequelize.query(`SELECT DISTINCT ( SELECT COUNT(*) FROM posts_comments WHERE posts_comments.forum_topic_id = posts.id ) AS comment_count, forum.forum_type, posts.createdAt AS postCreatedAt, posts.updatedAt AS postUpdatedAt, posts.id, posts.post_author_id, posts.post_title, posts.forum_type_id, users.user_login AS USER FROM forum, posts_comments, posts JOIN users ON posts.post_author_id = users.id WHERE posts.forum_type_id = forum.id ORDER BY ${orderBy} ${orderOption}`, { type: posts.sequelize.QueryTypes.SELECT }).then(posts => {
         res.status(200).send({ posts });
     }).catch(err => {
@@ -57,8 +56,25 @@ function adminUserDataUpdate(req, res) {
     });
 }
 
+function getUserModificable(req, res) {
+    var id = req.params.id;
+    users.sequelize.query("SELECT user_description, users.user_profile_image, users.id, users.user_login, users.user_email, users.user_status, users.user_rol, (SELECT COUNT(*) FROM posts where posts.post_author_id = users.id) AS post_count, (SELECT COUNT(*) FROM posts_comments WHERE posts_comments.post_comment_author_id = users.id) AS comment_count, (SELECT COUNT(*) FROM novels where novels.nvl_author = users.id) AS novel_count, (SELECT p.post_title FROM posts p where p.post_author_id=users.id ORDER BY createdAt DESC LIMIT 1) AS last_post FROM users WHERE users.id = ?", {
+        replacements: [id],
+        type: users.sequelize.QueryTypes.SELECT
+    }).then(user => {
+        res.status(200).send({
+            user
+        });
+    }).catch(err => {
+        res.status(500).send({
+            message: 'Ocurrio un error al buscar al usuario' + err
+        });
+    });
+}
+
 module.exports = {
     getAllPosts,
     adminUserDataUpdate,
-    adminVerification
+    adminVerification,
+    getUserModificable
 };
--- server/routes/adminPanel.js
+++ server/routes/adminPanel.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 const adminPanelController = require('../controllers').adminPanel;
 const md_auth = require('../authenticated/authenticated');
 
@@ -5,5 +6,5 @@ module.exports = (app) => {
     app.post('/api/admin-get-all-posts', md_auth.adminAuth, adminPanelController.getAllPosts);
     app.put('/api/admin-update-user', md_auth.adminAuth, adminPanelController.adminUserDataUpdate);
     app.post('/api/admin-verification', md_auth.adminAuth, adminPanelController.adminVerification);
-
+    app.get('/api/self-service-user/:id', md_auth.auth, adminPanelController.getUserModificable);
 };
