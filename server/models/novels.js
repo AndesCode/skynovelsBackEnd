3c5ad819da5b2ae03e60e1cfe0415bdc5f193bcf
Andres <andrecitoasd@gmail.com>
2019-11-26 16:43:48 +0100

    cambios en MyNovel

--- server/models/novels.js
+++ server/models/novels.js
@@ -11,7 +11,24 @@ module.exports = (sequelize, DataTypes) => {
         nvl_title: DataTypes.TEXT,
         nvl_status: DataTypes.CHAR,
         nvl_comment_status: DataTypes.CHAR,
-        nvl_name: DataTypes.CHAR,
+        nvl_name: {
+            type: DataTypes.CHAR,
+            validate: {
+                isUnique: function(value, next) {
+                    var self = this;
+                    novels.findOne({ where: { nvl_name: value } })
+                        .then(function(novels) {
+                            if (novels && self.id !== novels.id) {
+                                return next({ message: 'error, nombre de novela coincidente' });
+                            }
+                            return next();
+                        })
+                        .catch(function(err) {
+                            return next(err);
+                        });
+                }
+            }
+        },
         nvl_comment_count: DataTypes.INTEGER,
         nvl_writer: DataTypes.CHAR,
         nvl_img: DataTypes.CHAR,
