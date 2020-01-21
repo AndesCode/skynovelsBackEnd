9299edd4979b379ebc376f894da8389ffe6a9c53
Andres <andrecitoasd@gmail.com>
2020-01-21 09:07:26 +0100

    Llamando a las funciones correctas

--- server/routes/adminPanel.js
+++ server/routes/adminPanel.js
@@ -6,9 +6,9 @@ module.exports = (app) => {
     // Admin panel access
     app.get('/api/admin-panel', md_auth.adminAuth, adminPanelController.adminPanelAccess);
     // Forum categories
-    app.post('/api/admin-create-forum-category', md_auth.adminAuth, adminPanelController.createCategory);
-    app.put('/api/admin-update-forum-category', md_auth.adminAuth, adminPanelController.updateCategory);
-    app.delete('/api/admin-delete-forum-category/:id', md_auth.adminAuth, adminPanelController.deleteCategory);
+    app.post('/api/admin-create-forum-category', md_auth.adminAuth, adminPanelController.adminCreateCategory);
+    app.put('/api/admin-update-forum-category', md_auth.adminAuth, adminPanelController.adminUpdateCategory);
+    app.delete('/api/admin-delete-forum-category/:id', md_auth.adminAuth, adminPanelController.adminDeleteCategory);
     // forum posts
     app.put('/api/admin-update-forum-post', md_auth.adminAuth, adminPanelController.adminUpdatePost);
     app.delete('/api/admin-delete-forum-post/:id', md_auth.adminAuth, adminPanelController.adminDeletePost);
@@ -16,8 +16,8 @@ module.exports = (app) => {
     app.put('/api/admin-update-post-comment', md_auth.adminAuth, adminPanelController.adminUpdateComment);
     app.delete('/api/admin-delete-post-comment/:id', md_auth.adminAuth, adminPanelController.adminDeleteComment);
     // Users 
-    app.get('/api/admin-users/:status', md_auth.adminAuth, adminPanelController.getUsers);
-    app.delete('/api/admin-delete-user/:id', md_auth.adminAuth, adminPanelController.deleteUser);
+    app.get('/api/admin-users/:status', md_auth.adminAuth, adminPanelController.adminGetUsers);
+    app.delete('/api/admin-delete-user/:id', md_auth.adminAuth, adminPanelController.adminDeleteUser);
     // novels
     app.get('/api/admin-novels', md_auth.adminAuth, adminPanelController.adminGetNovels);
     app.put('/api/admin-update-novel', md_auth.adminAuth, adminPanelController.adminUpdateNovel);
