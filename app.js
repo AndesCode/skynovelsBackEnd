fd7c10ec76e23d2af571d9a249070c4c62cc0902
andresplzm02 <andrecitoasd@gmail.com>
2020-11-16 03:29:45 +0100

    cambios

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /*jshint esversion: 6 */
 require('dotenv').config();
 const http = require('http');
+const https = require('https');
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
@@ -10,30 +11,33 @@ const morgan = require('morgan');
 const passport = require('passport');
 const app = express();
 const helmet = require("helmet");
-const { NONAME } = require('dns');
+const config = require(__dirname + '/server/config/config.json');
+const fs = require('fs');
 
-const options = {
-    host: 'localhost',
-    user: 'root',
-    password: 'andres23722',
-    database: 'skynovelsdb_new',
-    expiration: 3024000000,
-    checkExpirationInterval: 14400000,
-    createDatabaseTable: true,
-    schema: {
-        tableName: 'sessions',
-        columnNames: {
-            session_id: 'sessionId',
-            expires: 'expires',
-            data: 'data'
-        }
-    }
+let sessionConfiguration;
+
+// key and certs 
+const key = fs.readFileSync(path.join(__dirname, '../ssl/keys/b488b_99ae9_e7c015ecebdcb61e95d824f2665262dc.key'), 'utf8');
+const cert = fs.readFileSync(path.join(__dirname, '../ssl/certs/skynovelstesting_a2hosted_com_b488b_99ae9_1608016588_dfa422c0b725d4023b8fea4978ad675b.crt'), 'utf8');
+
+const keyTest = fs.readFileSync(path.join(__dirname, '../ssltest/selfsigned.key'), 'utf8');
+const certTest = fs.readFileSync(path.join(__dirname, '../ssltest/selfsigned.crt'), 'utf8');
+
+const httpsOptions = {
+    key: key,
+    cert: cert
 };
 
-const sessionStore = new MySQLStore(options);
+const httpsOptionsTest = {
+    key: keyTest,
+    cert: certTest
+};
+
+console.log(httpsOptions);
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
     app.use(cors({ origin: true, credentials: true }));
+    sessionConfiguration = config.development_session;
 } else {
     /*const whitelist = ['https://skynovels.net'];
     const corsOptions = {
@@ -48,7 +52,10 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
     };
     app.use(cors(corsOptions));*/
     app.use(cors({ origin: true, credentials: true }));
+    sessionConfiguration = config.production_session;
 }
+
+const sessionStore = new MySQLStore(sessionConfiguration);
 require('./server/passport/local-auth');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -63,9 +70,14 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
         resave: false,
         store: sessionStore,
         saveUninitialized: false,
+        cookie: {
+            httpOnly: true,
+            maxAge: 3024000000
+        }
     }));
 } else {
     console.log('Environment: production test');
+    // NODE_ENV=production node app.js
     app.use(session({
         name: 'sessionId',
         secret: '$2b$24$ze2wpHDHn5muWBmiq4XMHuJgn7R4_YSm6b0MDGxjr.CME4YKOriWK',
@@ -73,11 +85,10 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
         store: sessionStore,
         saveUninitialized: false,
         cookie: {
-            secure: true,
+            // secure: true,
             httpOnly: true,
-            sameSite: 'none',
-            // domain: 'skynovels.net',
-            // path: 'foo/bar',
+            // sameSite: 'Strict', utilizar en servidor!!!
+            domain: 'skynovelstesting.a2hosted.com',
             maxAge: 3024000000
         }
     }));
@@ -117,7 +128,8 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
     });
 }
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
+const server = https.createServer(httpsOptions, app);
 let port;
 if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
     port = 3000;
@@ -125,7 +137,8 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
     port = 30000;
 }
 server.listen(process.env.PORT || port, function() {
-    console.log('running at http://localhost:' + server.address().port);
+    console.log(server.address().port ? 'running at http://localhost:' + server.address().port : 'running at http://localhost:' + process.env.PORT);
+    console.log(server.address());
 });
 
 module.exports = app;
--- server/models/index.js
+++ server/models/index.js
@@ -12,6 +12,7 @@ if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
+    console.log('Conectado a la base de datos: ' + config.database + ' con el usuario: ' + config.username);
 }
 
 fs
