eb201a06abda50f1d0b83d7d5ceda1f5d8048569
andresplzm02 <andrecitoasd@gmail.com>
2020-11-30 01:20:58 +0100

    cambios

--- app.js
+++ app.js
@@ -97,7 +97,11 @@ app.use(passport.session());
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
+        res.header('Access-Control-Allow-Origin', '*');
+    } else {
+        res.header('Access-Control-Allow-Origin', 'https://skynovelstesting.a2hosted.com');
+    }
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
